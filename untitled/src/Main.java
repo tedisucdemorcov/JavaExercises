a02639ab69562de5dc93410ba451555ad6f6b5e6
Calcii Cristian Teodor <110901389+tedisucdemorcov@users.noreply.github.com>
2023-06-07 15:49:15 +0300

    Java  begging
    New:
    - syntex
    - output
    - comments
    - variables

---
+++ untitled/src/Main.java
@@ -0,0 +1,18 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+//acestea sunt comentarii
+//        print text
+        System.out.println("Hello world!");
+        System.out.println("First time from now!");
+        System.out.println("I will become the best Java dev!");
+//        print numbers
+        System.out.println(3);
+        System.out.println(5);
+        System.out.println(5 * 3);
+        System.out.println(9+3);
+
+    }
+}
---
+++ untitled/src/Variables.java
@@ -0,0 +1,29 @@
+public class Variables {
+    public static void main(String args[])
+    {
+        System.out.println("String: stores text");
+        System.out.println("Int: stores integers, numbers");
+        System.out.println("Float: stores floating points with decimals");
+        System.out.println("Char: stores single characters, such as 'a' ");
+        System.out.println("Boolean: stores value with 2 states, true / false");
+
+        String name = "Mihai";
+        String account = "Personal";
+        int number = 5;
+        int first_number = 56;
+        float larger = 45;
+        double large = 1124.213;
+        char letter = 'a';
+        boolean select = true;
+
+        System.out.println(name);
+        System.out.println(account);
+        System.out.println(number);
+        System.out.println(first_number);
+        System.out.println(larger);
+        System.out.println(large);
+        System.out.println(letter);
+        System.out.println(select);
+
+    }
+}
